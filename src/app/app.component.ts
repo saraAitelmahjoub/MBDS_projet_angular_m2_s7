defb5c7b3ea3ddf2d7aefc970af1c3f5cd534f89
sara aitelmahjoub <saraitelmahjoub@gmail.com>
2023-01-16 11:38:33 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'assignment-app';
+  title = 'application de gestion de devoire a rendre ..';
 }
